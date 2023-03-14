ca67fe4cf3eeefb7f923cfa219a1e6a73dddc3d0
yildiztugba <okra.yildiz@gmail.com>
2023-03-14 11:03:13 +0100

    move modified notes to the top

--- src/App.js
+++ src/App.js
@@ -27,11 +27,19 @@ export default function App() {
     }
     
     function updateNote(text) {
-        setNotes(oldNotes => oldNotes.map(oldNote => {
-            return oldNote.id === currentNoteId
-                ? { ...oldNote, body: text }
-                : oldNote
-        }))
+         
+      setNotes(oldNotes => {
+            const newArray = []
+            for(let i = 0; i < oldNotes.length; i++) {
+                const oldNote = oldNotes[i]
+                if(oldNote.id === currentNoteId) {
+                    newArray.unshift({ ...oldNote, body: text })
+                } else {
+                    newArray.push(oldNote)
+                }
+            }
+            return newArray
+        })
     }
     
     function findCurrentNote() {
